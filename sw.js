53858859904759768dc576e076e3bbe64d7640ce
Hexon <hexon77.code@gmail.com>
2025-12-16 15:54:42 +0100

    added PWA support

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = 'pingpong-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
